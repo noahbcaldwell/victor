231cf39b257b47a6dbd922b10308d286e3a4461e
noahbcaldwell <107315487+noahbcaldwell@users.noreply.github.com>
2022-12-16 11:47:21 -0600

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+// Menu toggle
 $(document).ready(function(){
     $("#toggle-container").click(function(){
       $("#sidebar").css({"transition": "400ms", "margin-left": "0px"});
@@ -6,9 +7,12 @@ $(document).ready(function(){
       $("#sidebar").css({"transition": "400ms", "margin-left": "-350px"});
     });
 });
+
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
+
+// Smooth Scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();

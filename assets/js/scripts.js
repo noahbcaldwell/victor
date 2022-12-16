97bc018548bfd6d76636bc088ff755239f710f9e
noahbcaldwell <107315487+noahbcaldwell@users.noreply.github.com>
2022-12-16 10:51:31 -0600

    Added smooth scroll to hash links

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#toggle-container").click(function(){
+      $("#sidebar").css({"transition": "400ms", "margin-left": "0px"});
+    });
+    $("#toggle-container-close").click(function(){
+      $("#sidebar").css({"transition": "400ms", "margin-left": "-350px"});
+    });
+});
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
